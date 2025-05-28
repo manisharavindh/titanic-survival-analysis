7456e0df8ab90b91433f5096aaa8f5cd27c97606
Manish Aravindh <manisharavindh2007@gmail.com>
2025-05-28 19:57:45 +0530

    v0.3.0

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("data/train.csv")
+
+print(df.shape)
+print('----- ----- ----- ----- ----- -----')
+print(df.info())
+print('----- ----- ----- ----- ----- -----')
+print(df.head())
+print('----- ----- ----- ----- ----- -----')
+print(df.isnull().sum())
+print('----- ----- ----- ----- ----- -----')
+print(df.duplicated().sum())
+print('----- ----- ----- ----- ----- -----')
+print(df.describe())
